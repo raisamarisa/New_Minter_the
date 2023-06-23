b0ac39b309fc43929d60245589827cba65aad9c8
ame <sayedadmod@gmail.com>
2023-06-23 23:19:45 +0600

    first

---
+++ components/Home/Mint.tsx
@@ -0,0 +1,73 @@
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import useDirectCall from "../../hooks/useTransation";
+import { useAppSelector, useAppdispatch } from "../../hooks/redux";
+import { ConnectButtonwagmi, ExampleButton } from "../Header/connect";
+import { NFT_CONTRACT, NFT_PRICE } from "../../config";
+import { useAccount, useSigner, useSignMessage } from "wagmi";
+import { fetchTotalMint } from "../../hooks/Totalsupply";
+import { toast } from "react-hot-toast";
+import { HiPlus, HiMinus } from "react-icons/hi";
+import { provider } from "../../utils/providerweb3";
+export function Mint() {
+  const dispatch = useAppdispatch();
+  const { signMessageAsync, signMessage } = useSignMessage();
+  const { data: signer } = useSigner();
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+  const { BuyToken, loading, Checklimit } = useDirectCall(signer, NFT_CONTRACT);
+  const supply = useAppSelector((state) => state.wallet.totalSupply);
+  const [load, setload] = useState(false);
+  useEffect(() => {
+    setload(true);
+  }, []);
+
+  const Mintnft = async () => {
+  
+    if (!address) return toast.error("Connect your wallet");
+    const CheckLimitnft = await Checklimit("checkUserLimit",[address]);
+    if(CheckLimitnft){
+      const nonce = await fetch("/api/web3/nonce");
+      const nonceRes = await nonce.text();
+      const message = ethers.utils.solidityKeccak256(["string"], [nonceRes]);
+      const arrayifyMessage = await ethers.utils.arrayify(message);
+      const res = signMessageAsync({
+        message: arrayifyMessage,
+      });
+  
+      const signature = res.then((e) => {
+        // mint nere...
+         BuyToken('mint',[1,e,nonceRes]);
+      });
+    }else{
+      toast.error("You already minted!")
+    }
+    
+  };
+
+  useEffect(() => {
+    dispatch(fetchTotalMint());
+  }, [loading]);
+
+  return (
+    <div className="w-full flex flex-col justify-center  ">
+      <div className="py-2 text-center">
+        <h2 className="text-4xl text-white font-extrabold">{supply}/345</h2>
+        {/* <h2 className='text-lg uppercase py-2 text-white dark:text-white'>Price  {price?.toFixed(3)} ETH</h2> */}
+      </div>
+
+      <div className="flex flex-row justify-center gap-3 items-center py-2 mb-1"></div>
+
+      {load ? (
+        <div className="text-center">
+          <button
+            disabled={loading}
+            onClick={() => Mintnft()}
+            className="border  text-white font-bold py-2 px-10  tracking-[2px] font-Montserrat uppercase "
+          >
+            {loading ? "Minting.." : "Mint"}
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
---
+++ utils/providerweb3.ts
@@ -0,0 +1,37 @@
+import { WagmiConfig, createClient, mainnet } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
+
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { bsc,bscTestnet,goerli ,arbitrum} from "wagmi/chains";
+import { ethers } from "ethers";
+
+
+const RPC_URL = 'https://data-seed-prebsc-1-s2.binance.org:8545';
+export const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+
+// https://data-seed-prebsc-1-s2.binance.org:8545
+//const RPC_URL = 'https://eth-mainnet.g.alchemy.com/v2/9OFEz-mV6cQwYRPXIi75IW-NuOiKdtVU';
+
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+
+const alchemyId = "Tv277_RjwkXDuii_WGiG_X8RL-T56yyG";
+//  up client
+//
+const chains = [bscTestnet];
+
+export const client = createClient(
+    getDefaultClient({
+      appName: "minter",
+      alchemyId,
+      chains
+    }),
+  );
+
+// Pass client to React Context Provider
