d0483b934117070816ffeaaf422718784f9526b3
Y0h4n3s <yohaksa80@gmail.com>
2023-07-05 21:30:10 +0300

    style and favicon

--- components/Home/Mint.tsx
+++ components/Home/Mint.tsx
@@ -53,14 +53,13 @@ export function Mint() {
         {/* <h2 className='text-lg uppercase py-2 text-white dark:text-white'>Price  {price?.toFixed(3)} ETH</h2> */}
       </div>
 
-      <div className="flex flex-row justify-center gap-3 items-center py-2 mb-1"></div>
 
       {load ? (
         <div className="text-center">
           <button
             disabled={loading}
             onClick={() => Mintnft()}
-            className="border-[2px] border-[#29064e] text-[#29064e] bg-white  font-bold py-2 px-10  tracking-[2px] font-Montserrat uppercase "
+            className="border-[2px] border-[#29064e] rounded-2xl text-[#29064e] bg-white  font-bold py-2 px-10  tracking-[2px] font-Montserrat uppercase "
             >
             {loading ? "Minting.." : "Mint"}
           </button>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
       <body>
         <title>
-Site name</title>
+Empyreansaga</title>
         <Main />
         <NextScript />
       </body>
